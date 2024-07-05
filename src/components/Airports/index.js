18ca161963807ccf9390d657b678ed3543881810
mukeshlakkakula <lakkakulababblu@gmail.com>
2024-07-05 10:22:37 +0530

    removed airports table

--- src/components/Airports/index.js
+++ src/components/Airports/index.js
@@ -1,46 +1,8 @@
 import React from "react";
 import "./index.css";
-import {
-  Cell,
-  Column,
-  Row,
-  TableView,
-  TableBody,
-  TableHeader,
-} from "@adobe/react-spectrum";
 
 const Airports = () => {
-  let columns = [
-    { name: "Name", uid: "name" },
-    { name: "Type", uid: "type" },
-    { name: "Date Modified", uid: "date" },
-  ];
-  let rows = [
-    { id: 1, name: "Games", date: "6/7/2020", type: "File folder" },
-    { id: 2, name: "Program Files", date: "4/7/2021", type: "File folder" },
-    { id: 3, name: "bootmgr", date: "11/20/2010", type: "System file" },
-    { id: 4, name: "log.txt", date: "1/18/2016", type: "Text Document" },
-  ];
-  return (
-    <TableView
-      aria-label="Example table with dynamic content"
-      maxWidth="size-6000"
-    >
-      <TableHeader columns={columns}>
-        {(column) => (
-          <Column
-            key={column.uid}
-            align={column.uid === "date" ? "end" : "start"}
-          >
-            {column.name}
-          </Column>
-        )}
-      </TableHeader>
-      <TableBody items={rows}>
-        {(item) => <Row>{(columnKey) => <Cell>{item[columnKey]}</Cell>}</Row>}
-      </TableBody>
-    </TableView>
-  );
+  return <div>Airports</div>;
 };
 
 export default Airports;
--- src/components/Body/index.js
+++ src/components/Body/index.js
@@ -1,18 +1,11 @@
 import React from "react";
 import "./index.css";
-import { FileTrigger, Button } from "@adobe/react-spectrum";
 import Airports from "../Airports";
+
 const Body = () => {
   return (
     <div>
-      <div>
-        <Airports />
-      </div>
-      <FileTrigger acceptedFileTypes={["image/png"]}>
-        <Button variant="primary" style={{ width: "150px" }}>
-          Select files
-        </Button>
-      </FileTrigger>
+      <Airports />
     </div>
   );
 };
